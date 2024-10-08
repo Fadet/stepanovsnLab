16c3b75de3351f0527da5458ec8502b9b12db1d3
Fadet <tntboxmc@gmail.com>
2024-10-08 16:02:06 +0300

    lab 1

---
+++ src/main/java/tech/reliab/course/stepanovsnLab/bank/stepanovsnLabApplication.java
@@ -0,0 +1,7 @@
+package tech.reliab.course.stepanovsnLab.bank;
+
+public class stepanovsnLabApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
